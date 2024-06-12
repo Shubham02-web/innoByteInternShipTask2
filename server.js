eebcc6f3c975aaf7323d19c27afa6ef7210c5699
Shubham Vishwakarma <vi453770@gmail.com>
2024-06-13 02:00:31 +0530

    completed the 2nd task and now have done internship for a backend developer role at innoByte it is same as first task the diffrence is that is advance as compare to first task as in these we have add a functionality to sending and conforming a email link or code

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
 const cookie = require("cookie-parser");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cookie());
 mongoose
   .connect(process.env.MongoURI)
@@ -23,6 +28,31 @@ const schema = mongoose.Schema({
 });
 
 const user = new mongoose.model("InternshipCollection", schema);
+
+// creating transporter Email
+
+const transPorter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.YourEmail,
+    pass: process.env.YourPass,
+  },
+});
+
+const conformationMail = (userEmail, conformationCode) => {
+  const mailOptions = {
+    from: process.env.YourEmail,
+    to: userEmail,
+    subject: "Email Conformation",
+    html: `<h1>Email Conformation</h1>
+    <p>Please Confirm your Email By Clicking the below link</p>,
+    <a href="http:ShubhamProject.com/confirm?code=${conformationCode}">Click Me</a>`,
+  };
+  transPorter.sendMail(mailOptions, (err, info) => {
+    if (err) console.log(`error in sending email`, err);
+    else console.log(`email sent`, info.response);
+  });
+};
 app.post("/api/signup", async function (req, res) {
   try {
     const { userName, email, password } = req.body;
@@ -33,16 +63,21 @@ app.post("/api/signup", async function (req, res) {
       });
 
     const hashedPass = await bcrypt.hash(password, 10);
+    const conformationCode = crypto.randomBytes(20).toString("hex");
     const temp = await user.create({
       userName,
       email,
       password: hashedPass,
+      conformationCode,
     });
+
     temp.save();
+    conformationMail(email, conformationCode);
     temp.password = undefined;
     res.status(201).send({
       success: true,
-      message: "User Created Succesfully",
+      message:
+        "User Created Succesfully please check your email to conform your sign up",
       temp,
     });
   } catch (error) {
@@ -53,6 +88,27 @@ app.post("/api/signup", async function (req, res) {
   }
 });
 
+app.get("/conformation", async function (req, res) {
+  try {
+    const { code } = req.query;
+    const User = await user.findOne({ conformationCode: code });
+    if (!User) return res.status(500).send("Invalid Conformation Code");
+
+    //   Clear Conformation code
+    User.conformationCode = "";
+    await User.save();
+    res.status(200).send({
+      success: true,
+      message: "Email Conformation Successful",
+      User,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Error in Confirm Email API",
+    });
+  }
+});
 app.get("/api/login", async function (req, res) {
   try {
     const { email, password } = req.body;
